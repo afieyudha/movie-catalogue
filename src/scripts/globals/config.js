f9e3e34985d11355d3ef0d3f9e2a0fca82e6b98e
afieyudha <ayudhatriadi20@gmail.com>
2021-06-12 22:46:26 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const CONFIG = {
+    KEY: 'e788c39f155ea5d5e0f5123bc2b8537c',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+    DATABASE_NAME: 'movie-catalogue-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'movies',
+    WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+  };
+
+  export default CONFIG;
